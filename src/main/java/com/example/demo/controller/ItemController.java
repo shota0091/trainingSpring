6afb978f50d2a0d1100df6fce446ce3e07a999a9
shota0091 <syou0091@yahoo.co.jp>
2023-03-25 14:18:12 +0900

    サービスクラスの作成

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,22 +1,26 @@
 package com.example.demo.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.repository.ItemRepository;
+import com.example.demo.entity.Item;
+import com.example.demo.service.ItemService;
 
 @Controller
 public class ItemController {
 
 	@Autowired
-	ItemRepository itemrepositoy;
-	
+	ItemService itemService;
 	
 	@RequestMapping("/index")
 	public String index(Model model) {
-		model.addAttribute("items",itemrepositoy.findAll());
+		List<Item> items = itemService.serachAll();
+		model.addAttribute("items",items);
 		return "index";
 	}
 }
---
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Item;
+import com.example.demo.repository.ItemRepository;
+
+@Service
+public class ItemService {
+	
+	
+	@Autowired
+	ItemRepository itemRepository;
+	
+	
+	public List<Item> serachAll(){
+		return itemRepository.findAll();
+	}
+
+}
