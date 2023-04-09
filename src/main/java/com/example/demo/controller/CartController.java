4eab191b6056d9e1feeb4347793215fe8c68c1ec
shota0091 <syou0091@yahoo.co.jp>
2023-04-09 15:37:27 +0900

    中間

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Item;
+import com.example.demo.itemFrom.CartFrom;
 import com.example.demo.repository.ItemRepository;
 import com.example.demo.service.ItemService;
 
@@ -27,22 +27,48 @@ public class CartController {
 	@Autowired
 	ItemRepository itemRepository;
 	
-	List<Item> basketList = new ArrayList<Item>();
+	ArrayList<Item> basketList = new ArrayList<>();
 	// 買い物かご
 	@RequestMapping("/item/itemBox")
-	public String box() {
+	public String box(CartFrom from) {
 		session.setAttribute("baskes",basketList);
 		return "html/itemBox";
 	}
 
 	// 買い物籠追加
 	@RequestMapping("/item/itemBox/add")
-	public String boxAdd(@ModelAttribute Item item) {
-		basketList.add(item);
+	public String boxAdd(@ModelAttribute Item item,CartFrom from) {
+		item.setQuantity(from.getPurchaseQuantity());
+		int id1 = basketList.lastIndexOf(item);
+		
+		System.out.println(id1);
+		if(basketList.isEmpty()) {
+			basketList.add(item);
+		}else if(id1 < 0) {
+			if(basketList.contains(item) == false) {
+				basketList.add(item);
+				System.out.println(222123);
+			}else {
+				System.out.println(123);
+			}
+			
+		}else if(id1 > 0){
+			int i = basketList.get(id1).getId();
+			if(i == item.getId()) {
+				System.out.println(456);
+			}else {
+				System.out.println(789);
+				basketList.add(item);
+			}
+			
+			
+		}
+			
+//			System.out.println(basketList);
 		return "redirect:/item/itemBox";
 	}
 	
-	// 買い物籠追加
+	// 買い物籠削除
 	@RequestMapping("/item/itemBox/remove/{id}")
 	public String remove(@PathVariable int id) {
 		basketList.remove(id);
